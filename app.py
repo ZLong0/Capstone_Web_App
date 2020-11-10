adbd1a96dbb1e2077387dfdc101b47789bbdff0e
C-Azzone416 <42585558+C-Azzone416@users.noreply.github.com>
2020-11-10 17:05:40 -0500

    fixed local imports

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3  
 from flask import Flask, render_template, json, request
-#from flask.ext.mysql import MySQL
+from flaskext.mysql import MySQL
 
-#mysql = MySQL()
+
+mysql = MySQL()
 app = Flask(__name__)
 
 # MySQL configurations
-#app.config['MYSQL_DATABASE_USER'] = '*'
-#app.config['MYSQL_DATABASE_PASSWORD'] = '*'
-#app.config['MYSQL_DATABASE_DB'] = '*'
-#app.config['MYSQL_DATABASE_HOST'] = '*'
-#mysql.init_app(app)
+app.config['MYSQL_DATABASE_USER'] = '*'
+app.config['MYSQL_DATABASE_PASSWORD'] = '*'
+app.config['MYSQL_DATABASE_DB'] = '*'
+app.config['MYSQL_DATABASE_HOST'] = '*'
+mysql.init_app(app)
 
 
 @app.route("/", methods=["POST", "GET"])
