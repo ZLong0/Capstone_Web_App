60edc42b28f2afe74553101f8965c1f40ce03848
C-Azzone416 <42585558+C-Azzone416@users.noreply.github.com>
2020-11-10 12:17:16 -0500

    updates to app.py

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3  
 from flask import Flask, render_template, json, request
-from flaskext.mysql import MySQL
+#from flask.ext.mysql import MySQL
 
-mysql = MySQL()
+#mysql = MySQL()
 app = Flask(__name__)
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = '*'
-app.config['MYSQL_DATABASE_PASSWORD'] = '*'
-app.config['MYSQL_DATABASE_DB'] = '*'
-app.config['MYSQL_DATABASE_HOST'] = '*'
-mysql.init_app(app)
+#app.config['MYSQL_DATABASE_USER'] = '*'
+#app.config['MYSQL_DATABASE_PASSWORD'] = '*'
+#app.config['MYSQL_DATABASE_DB'] = '*'
+#app.config['MYSQL_DATABASE_HOST'] = '*'
+#mysql.init_app(app)
 
 
-@app.route('/')
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")  # this should be the name of your html file
+    return render_template("login.html")  # this should be the name of your html file
+
+
+@app.route("/home", methods=["POST", "GET"])
+def home():
+    return render_template("home.html")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    return render_template("login.html")
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='80')
+    app.run(debug=True)
