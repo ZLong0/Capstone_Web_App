80bd9792383aaf86ac71ed3e904d3d8b076148b5
C-Azzone416 <42585558+C-Azzone416@users.noreply.github.com>
2020-11-14 19:04:53 -0500

    GET methods for grabbing all items

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3  
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, jsonify, request, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from sqlalchemy import create_engine
+from dataclasses import dataclass
 
 app = Flask(__name__)
 app.secret_key = "capstonefall2020"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///atas.sqlite3'
 db = SQLAlchemy(app)
 
+
+#DATABASE MODELS ARE FOLLOWED DIRECTLY BY THE METHODS THAT CORRESPOND TO THOSE OPJECTS
+#IF TIME ALLOWS - MOVE MODELS TO THEIR OWN MODELS.PY FILE
 
 class Users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
@@ -15,10 +21,10 @@ class Users(db.Model):
     is_Admin = db.Column("admin", db.Boolean(0))
     is_active = db.Column(db.Boolean(0))
 
-    def __init__(self, username, password, is_admin, is_active):
+    def __init__(self, username, password, is_Admin, is_active):
         self.username = username
         self.password = password
-        self.is_Admin = is_admin
+        self.is_Admin = is_Admin
         self.is_active = is_active
 
 
@@ -31,7 +37,22 @@ class Instructor(db.Model):
         self.fname=fname
         self.lname = lname
 
- 
+
+@app.route('/instructors', methods=['GET'])
+def get_all_instructors():
+    instructors = Instructor.query.all()
+    results = []
+
+    for instructor in instructors:
+        instructor_data = {}
+        instructor_data['instructor_id'] = instructor.inst_id
+        instructor_data['first_name'] = instructor.fname
+        instructor_data['last_name'] = instructor.lname
+        results.append(instructor_data)
+    
+    return jsonify(results)
+
+
 class Student(db.Model):
     student_id = db.Column("id",db.Integer, primary_key=True)
     fname = db.Column(db.String(100))
@@ -40,6 +61,21 @@ class Student(db.Model):
     def __init__(self, fname, lname):
         self.fname = fname
         self.lname = lname
+
+
+@app.route('/students', methods=['GET'])
+def get_all_students():
+    students = Student.query.all()
+    results = []
+
+    for student in students:
+        student_data = {}
+        student_data['student_id'] = student.student_id
+        student_data['first_name'] = student.fname
+        student_data['last name'] = student.lname
+        results.append(student_data)
+    
+    return jsonify(results)
 
 
 class Course(db.Model):
@@ -62,6 +98,24 @@ class Course(db.Model):
         self.instructor = instructor
 
 
+@app.route('/courses', methods =["GET"])
+def get_all_courses():
+    courses = Course.query.all()
+    results = []
+    
+    for course in courses:
+        course_info = {}
+        course_info['course_name'] = course.course_name
+        course_info['term'] = course.term
+        course_info['year'] = course.year
+        course_info['course_department'] = course.department
+        course_info['course_number'] = course.course_number
+        course_info['section'] = course.section
+        results.append(course_info)
+
+    return jsonify(results)
+
+
 class Outcomes(db.Model):
     so_id = db.Column("so_id",db.Integer, primary_key=True)
     so_name = db.Column(db.String(4))
@@ -70,6 +124,23 @@ class Outcomes(db.Model):
     def __init__(self, so_name, so_desc):
         self.so_name = so_name
         self.so_desc = so_desc
+
+
+@app.route("/outcomes", methods=["GET"])
+#this GET queries the db then returns a json object of information to the /outcomes endpoint.  this is a static list currently so not 'POST' method 
+
+def get_all_outcomes():
+    outcomes = Outcomes.query.all()
+
+    results = []
+
+    for outcome in outcomes:
+        outcome_data = {}
+        outcome_data['so_name'] = outcome.so_name
+        outcome_data['so_desc'] = outcome.so_desc
+        results.append(outcome_data)
+    
+    return jsonify(results)
 
 
 class Assignments(db.Model):
@@ -82,17 +153,46 @@ class Assignments(db.Model):
         self.swp_name = swp_name
 
 
+@app.route('/swp', methods=["GET"])
+def get_all_swp():
+    swps = Assignments.query.all()
+
+    results = []
+
+    for swp in swps:
+        swp_data = {}
+        swp_data['swp_id'] = swp.swp_id
+        swp_data['swp_name'] = swp.swp_name
+        swp_data['course_id'] = swp.course_id
+        results.append(swp_data)
+    
+    return jsonify(results)
+
 
 class Attempts(db.Model):
     attempt_id = db.Column("id", db.Integer, primary_key=True)
     swp_id = db.Column(db.Integer, db.ForeignKey('assignments.swp_id'))
     so_id = db.Column(db.Integer, db.ForeignKey('outcomes.so_id'))
 
-
-
     def __init__(self, swp_id, so_id):
         self.swp_id = swp_id
         self.so_id = so_id
+
+
+@app.route('/attempts', methods=["GET"])
+def get_all_attempts():
+    attempts = Attempts.query.all()
+
+    results = []
+
+    for attempt in attempts:
+        attempt_data = {}
+        attempt_data['attempt_id'] = attempt.attempt_id
+        attempt_data['swp'] = attempt.swp_id
+        attempt_data['so'] = attempt.so_id
+        results.append(attempt_data)
+    
+    return jsonify(results)
 
 
 class Enrolled(db.Model):
@@ -105,6 +205,7 @@ class Enrolled(db.Model):
         self.course_id = course_id
 
 
+
 class Results(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
@@ -113,6 +214,7 @@ class Results(db.Model):
     def __init__(self, student_id, attempt_id):
         self.student_id = student_id
         self.attempt_id = attempt_id
+
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -133,12 +235,6 @@ def register():
 @app.route("/logout")
 def logout():
     return render_template("login.html")
-
-
-
-@app.route("/outcomes")
-def outcomes():
-    return {'so_name': 'SO1', 'so_desc': 'this is a test'}
 
 
 if __name__ == '__main__':
