5e41f50d5234b11a47c845c6b7d5faa231107830
C-Azzone416 <42585558+C-Azzone416@users.noreply.github.com>
2020-11-30 14:17:22 -0500

    updated student/instructor endpoints.  tested using postman  200 codes returned.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, jsonify, request, session, redirect, f
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
-from sqlalchemy import create_engine, distinct, func
+from sqlalchemy import create_engine, distinct, func, delete, insert, update
 from flask_mail import Mail, Message
 
 app = Flask('__name__')
@@ -13,6 +13,7 @@ db = SQLAlchemy(app)
 engine = create_engine('sqlite:///atas.sqlite3')
 login_manager = LoginManager(app)
 mail = Mail(app)
+
 
 app.config['SECRET_KEY'] = 'capstonefall2020'
 app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
@@ -97,8 +98,6 @@ def index():
     
     if not active:
         return render_template("login.html")
-    elif active.account_type == 'instructor':
-        return redirect(url_for('instructor_home'))
     else:
         return redirect(url_for('home'))
 
@@ -260,7 +259,7 @@ class Instructor(db.Model):
 
 
 @app.route('/instructors', methods=['GET'])
-@login_required
+#@login_required
 def get_instructors():
     instructors = Instructor.query.all()
     results = []
@@ -275,36 +274,108 @@ def get_instructors():
     return jsonify(results)
 
 
-@app.route('/instructors/<instructor_id>', methods=['POST'])
-@login_required
+@app.route('/instructors/<int:instructor_id>', methods=['GET'])
+#@login_required
+def get_one_instructor(instructor_id):
+    instructor = Instructor.query.get(instructor_id)
+    results = []
+    instructor_data = {}
+    instructor_data['instructor_id'] = instructor.inst_id
+    instructor_data['first_name'] = instructor.fname
+    instructor_data['last_name'] = instructor.lname
+    results.append(instructor_data)
+
+    return jsonify(results)
+
+
+@app.route('/instructors/<instructor_id>', methods=['POST', 'GET'])
+#@login_required
 def update_instructors(instructor_id):
     if request.method == 'POST':
-        # TODO: UPDATE THIS TO TAKE IN VALUE FROM FORM
-        id = 1
         # check for existing instructor id first
-        instructor = Instructor.query.filter_by(id=instructor_id).first()
-        if instructor:
-            return 'This employee ID is already registered'
+        instructor = Instructor.query.get(instructor_id)
+        if instructor:         
+            instructor.fname = request.form['first_name']
+            instructor.lname = request.form['last_name']
+            db.session.commit()
+            print('Instructor Updated!')
+            return redirect(url_for('get_instructors'))
         else:
-            ""
-    return ""
+            print("Instructor not found. Cannot update")
+
+
+@app.route('/instructors', methods=['PUT', 'GET'])
+#@login_required
+def add_instructor():
+    if request.method == 'PUT':
+        # check for existing instructor id first
+        try:
+            instructor = Instructor.query.get(request.form['id'])
+        except:
+            print("EXCEPTION ERROR")
+            return redirect(url_for('get_instructors')) 
+        
+        if instructor:         
+            print("instructor found")
+            return redirect(url_for('get_instructors'))
+        else:
+            print("instructor not found -- add")            
+            id = request.form['id']
+            print(id)
+            fname = request.form['first_name']
+            print(fname)
+            lname = request.form['last_name']
+            print(lname)
+            instructor = Instructor(id, fname, lname)
+            
+            if instructor: 
+                print("instructor created")
+                instructor_data={}
+                result =[]
+                instructor_data['first'] = fname
+                instructor_data['last'] = lname
+                instructor_data['id'] = id
+                result.append(instructor_data)
+                print (result)
+            dbconnection = engine.connect()                
+            statement = f"INSERT INTO instructor(id, fname, lname) VALUES ({id}, '{fname}','{lname}');"
+            print (statement)
+            dbconnection.execute(statement)
+            dbconnection.close()
+            return redirect(url_for('get_instructors')) 
+
+
+@app.route('/instructors/<instructor_id>', methods=['DELETE', 'GET'])
+#@login_required
+def delete_instructor(instructor_id):
+    if request.method == 'DELETE':
+        # check for existing instructor id first
+        instructor = Instructor.query.get(instructor_id)
+        if instructor:         
+            db.session.delete(instructor)
+            db.session.commit()
+            print("Instructor deleted " + instructor_id)
+            return redirect(url_for('get_instructors'))
+        else:
+            print("Instructor not found. Cannot delete")
 
 
 # STUDENT CLASS
 class Student(db.Model):
-    student_id = db.Column("id", db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     fname = db.Column(db.String(100))
     lname = db.Column(db.String(100))
 
-    def __init__(self, fname, lname):
+    def __init__(self, student_id, fname, lname):
         self.fname = fname
         self.lname = lname
+        self.stude_id = student_id
 
 
 @app.route('/students', methods=['GET'])
-@login_required
+#@login_required
 def get_all_students():
-    students = Student.query.group_by('lname').all()
+    students = Student.query.all()
     results = []
     for student in students:
         student_data = {}
@@ -312,17 +383,32 @@ def get_all_students():
         student_data['first_name'] = student.fname
         student_data['last name'] = student.lname
         results.append(student_data)
+    return jsonify(results)
+
+
+@app.route('/students/<int:student_id>', methods=['GET'])
+#@login_required
+def get_one_student(student_id):
+    student = Student.query.get(student_id)
+    if not student:
+        return "Student not found"
+    results = []
+    student_data = {}
+    student_data['student_id'] = student.student_id
+    student_data['first_name'] = student.fname
+    student_data['last name'] = student.lname
+    results.append(student_data)
 
     return jsonify(results)
 
 
 @app.route('/students/<student_id>', methods=['GET', 'POST'])
-@login_required
-def update_student():
+#@login_required
+def update_student(student_id):
     if request.method == 'POST':
         try:
             # get course to update by ID save in var
-            student = Student.query.get(request.form['id'])
+            student = Student.query.get(student_id)
 
             # update existing course info based on form input
             student.fname = request.form['first_name']
@@ -331,52 +417,72 @@ def update_student():
             # commit changes to db
             db.session.commit()
             flash('Student Updated!')
-            return redirect(url_for('home'))
+            #return redirect(url_for('home'))
+            return redirect(url_for('get_all_students'))
         except:
             flash('Update failed!')
             return redirect(url_for('get_all_courses'))
 
-    return render_template('/home')
+    #return render_template('/home')
 
-    return ""
+    return redirect(url_for('get_all_students'))
 
 
 @app.route('/students', methods=['GET', 'PUT'])
-@login_required
+#@login_required
 def add_students():
     if request.method == 'PUT':
         try:
             # get course to update by ID save in var
             student = Student.query.get(request.form['id'])
-
         except:
-            # commit changes to db
-            student = Student()
-            student.id = request.form['id']
-            student.fname = request.form['first_name']
-            student.lname = request.form['last_name']
-            db.session.add(student)
-            db.session.commit()
-            flash('Student Added!')
-            return redirect(url_for('get_all_courses'))
+            print("EXCEPTION ERROR")
+            return redirect(url_for('get_all_students'))
+        if student:
+            print("student found")
+            return redirect(url_for('get_all_students'))
+        else:
+            print("student not found -- add student")            
+            student_id = request.form['id']
+            print(student_id)
+            fname = request.form['first_name']
+            print(fname)
+            lname = request.form['last_name']
+            print(lname)
+            student = Student(student_id, fname, lname)
+            
+            if student: 
+                print("student created")
+                student_data={}
+                result =[]
+                student_data['first'] = fname
+                student_data['last'] = lname
+                student_data['id'] = student_id
+                result.append(student_data)
+                print (result)
+            print("work you stupid thing")
+            dbconnection = engine.connect()                
+            statement = f"INSERT INTO student(student_id, fname, lname) VALUES ({student_id}, '{fname}','{lname}');"
+            print (statement)
+            dbconnection.execute(statement)
+            dbconnection.close()
+            return redirect(url_for('get_all_students'))
+      
 
-    return render_template('/home')
 
-
-@app.route('/students/<student_id>', methods=['GET', 'DELETE'])
-@login_required
+@app.route('/students/<int:student_id>', methods=['GET', 'DELETE'])
+#@login_required
 def delete_students(student_id):
     if request.method == 'DELETE':
         try:
             student = Student.query.get(student_id)
             db.session.delete(student)
-            db.commit()
-
-            flash("Student Deleted")
+            db.session.commit()
+            print("Student deleted" + student_id)
         except:
-            flash("Error:  Delete Unsuccessful")
+            print("Error:  Delete Unsuccessful")
 
-    return redirect(url_for('home'))
+    return redirect(url_for('get_all_students'))
 
 
 # COURSE CLASS
